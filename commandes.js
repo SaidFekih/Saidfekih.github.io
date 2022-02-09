046ceffe7d580ebbaedf61f80ca0022252cf19de
Said Fekih <saidgwx@gmail.com>
2022-02-08 23:00:17 -0500

    mise en ligne premier site

---
+++ commandes.js
@@ -0,0 +1,12 @@
+//fichier non utilisé pour le moment.
+
+function getResolution() {
+   let screenWidth = screen.width ;
+   return(screenWidth)
+  }
+
+function switchMenu(){
+    let widthvalue = getResolution();
+    alert(widthvalue);
+
+}
